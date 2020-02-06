76ae50a159dee28fb0daea27c83667ee5d587de2
Lucas Kalinowski <lkalinowski@cwb1-kfanfd57.latam.corp.jwt.com>
2020-02-06 09:32:24 -0300

    Updates

---
+++ precache-manifest.a508c071a6ea01426d5b9f72a65637cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5e387931d3697049f4df635bb34bca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4c8018c2b0433ec0703",
+    "url": "./static/css/main.95cd2ba7.chunk.css"
+  },
+  {
+    "revision": "72fdae65fde553685d97",
+    "url": "./static/js/2.2793dcb5.chunk.js"
+  },
+  {
+    "revision": "e4c8018c2b0433ec0703",
+    "url": "./static/js/main.d5630078.chunk.js"
+  },
+  {
+    "revision": "889cb867948cbe08b07d",
+    "url": "./static/js/runtime-main.fde1254e.js"
+  },
+  {
+    "revision": "ee6fb908263f8ca6f817450013dadef0",
+    "url": "./static/media/arrow-drop-down.ee6fb908.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-training-myreads/precache-manifest.2a652c09265f4baa3264169109a318c0.js"
+  "./precache-manifest.a508c071a6ea01426d5b9f72a65637cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-training-myreads/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
